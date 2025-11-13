14b94f9ed0a4b97969d0e93f59ed50b05c6a8fd1
subhanmeer <banimeer@gmail.com>
2025-11-13 05:06:24 -0800

    Fix missing Link import in Login.js

--- src/landing_page/login/Login.js
+++ src/landing_page/login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "./Login.css"; // Create this CSS file
 
